8f8a8b176fec426bb23b1543d99d5c920dc037ba
czsm <2209924865@qq.com>
2022-10-28 01:44:37 +0800

    第一次提交，初始化项目，完成基础框架搭建

---
+++ czsm-blog/src/views/album/Album.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>相册</div>
+</template>
+
+<script>
+export default {
+  name: "Album"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ czsm-blog/src/views/talk/Talk.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>说说</div>
+</template>
+
+<script>
+export default {
+  name: "Talk"
+}
+</script>
+
+<style scoped>
+
+</style>
